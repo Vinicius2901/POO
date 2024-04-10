ab86959639bf83aef10e9495ce6b4d57ded21a49
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2024-04-09 23:18:27 -0300

    Add files via upload

---
+++ Lista2/Exercicio1/src/negocio/listaPessoas.java
@@ -0,0 +1,15 @@
+package negocio;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dados.Pessoa;
+
+public class listaPessoas {
+	private Map<String, List<Pessoa>> pessoas = new HashMap<String, List<Pessoa>>();
+	
+	public listaPessoas() {
+		
+	}
+}
